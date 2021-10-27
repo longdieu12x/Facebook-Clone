8578cf26227be7cee8487b325059690abb6bc897
Your Name <longdieu12x@gmail.com>
2021-10-27 22:57:19 +0700

    27/10/2021

--- server/index.js
+++ server/index.js
@@ -33,14 +33,14 @@ const storage = multer.diskStorage({
 		cb(null, "public/images");
 	},
 	filename: (req, file, cb) => {
-		cb(null, req.body.name);
+		cb(null, file.originalname);
 	},
 });
 
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
 	try {
-		return res.status(200).json("File uploded successfully");
+		return res.status(200).json("File uploaded successfully");
 	} catch (error) {
 		console.error(error);
 	}
